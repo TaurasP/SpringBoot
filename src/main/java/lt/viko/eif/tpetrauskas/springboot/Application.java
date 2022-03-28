0b000d386cc48de3cc721543599e8f8ca313c2fc
Tauras Petrauskas <petrauskas.post@gmail.com>
2022-03-28 10:29:23 +0300

    Added log4j2

--- src/main/java/lt/viko/eif/tpetrauskas/springboot/Application.java
+++ src/main/java/lt/viko/eif/tpetrauskas/springboot/Application.java
@@ -1,5 +1,7 @@
 package lt.viko.eif.tpetrauskas.springboot;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
+    private static final Logger LOG = LogManager.getLogger(Application.class);
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
+        LOG.debug("This Will Be Printed On Debug");
+        LOG.info("This Will Be Printed On Info");
+        LOG.warn("This Will Be Printed On Warn");
+        LOG.error("This Will Be Printed On Error");
+        LOG.fatal("This Will Be Printed On Fatal");
+
+        LOG.info("Appending string: {}.", "Hello, World");
     }
 
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+        LOG.info("Appending string: {}.", "Hello, World");
         return String.format("Hello %s!", name);
     }
 }
--- src/main/java/lt/viko/eif/tpetrauskas/springboot/ConsumingRestApplication.java
+++ src/main/java/lt/viko/eif/tpetrauskas/springboot/ConsumingRestApplication.java
@@ -14,21 +14,27 @@ public class ConsumingRestApplication {
 
     private static final Logger log = LoggerFactory.getLogger(ConsumingRestApplication.class);
 
-    public static void main(String[] args) {
-        SpringApplication.run(ConsumingRestApplication.class, args);
-    }
-
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
     }
 
-    @Bean
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumingRestApplication.class, args);
+
+
+    RestTemplate restTemplate = new RestTemplate();
+    Quote quote = restTemplate.getForObject("https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+
+    log.info(quote.toString());
+
+    /*@Bean
     public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
         return args -> {
             Quote quote = restTemplate.getForObject(
                     "https://quoters.apps.pcfone.io/api/random", Quote.class);
             log.info(quote.toString());
         };
+    }*/
     }
 }
