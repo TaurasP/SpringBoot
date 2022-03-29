5cea43590525346cdf68e58de0e5f90308a4e02d
Tauras Petrauskas <petrauskas.post@gmail.com>
2022-03-29 09:57:33 +0300

    Added swagger

--- src/main/java/lt/viko/eif/tpetrauskas/springboot/ConsumingRestApplication.java
+++ src/main/java/lt/viko/eif/tpetrauskas/springboot/ConsumingRestApplication.java
@@ -2,13 +2,13 @@ package lt.viko.eif.tpetrauskas.springboot;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+// API URL is closed - not reachable
 @SpringBootApplication
 public class ConsumingRestApplication {
 
@@ -27,14 +27,5 @@ public class ConsumingRestApplication {
     Quote quote = restTemplate.getForObject("https://gturnquist-quoters.cfapps.io/api/random", Quote.class);
 
     log.info(quote.toString());
-
-    /*@Bean
-    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
-        return args -> {
-            Quote quote = restTemplate.getForObject(
-                    "https://quoters.apps.pcfone.io/api/random", Quote.class);
-            log.info(quote.toString());
-        };
-    }*/
     }
 }
